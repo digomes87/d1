85c6a68eb8327258dc5e0fbd01ff22f7bf86dda7
Diego Go <dgomes@Diegos-MacBook-Pro.local>
2021-05-23 09:37:26 -0300

    Fix all erros for staticfiles

--- d1/settings.py
+++ d1/settings.py
@@ -9,7 +9,7 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=lambda v: [s.strip() for s in v.split(',')]) # noqa pylint: disable=not-callable
 
 
 # Application definition
@@ -90,7 +90,9 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 LOGOUT_REDIRECT_URL = 'index'
