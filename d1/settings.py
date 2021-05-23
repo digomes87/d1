dcca64ad8652ddaea88e27d50a5b4aab205c5e97
Diego Go <dgomes@Diegos-MacBook-Pro.local>
2021-05-23 09:41:12 -0300

    Add pylintrc again

--- d1/settings.py
+++ d1/settings.py
@@ -9,7 +9,7 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=lambda v: [s.strip() for s in v.split(',')]) # noqa pylint: disable=not-callable
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=lambda v: [s.strip() for s in v.split(',')])  # noqa pylint: disable=not-callable
 
 
 # Application definition
